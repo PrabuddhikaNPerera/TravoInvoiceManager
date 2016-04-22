ea4f5e07593417aab51303cb7acec5dfeb12b61c
PrabuddhikaNPerera <prabuddhike@gmail.com>
2016-04-22 18:18:06 +0530

    Shashika's modification Create two separate fxml to open invoice and new invoice

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,4 +1,12 @@
 package Controller;
 
+import javafx.scene.control.Button;
+
 public class Controller {
+    public Button addInvoice;
+
+    public void setAddInvoice(){
+        System.out.println("ok");
+    }
+
 }
